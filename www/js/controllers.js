8831743ea6d963b02bc008792fb2217f096353fa
otelnov <otelnov@gmail.com>
2015-04-02 14:29:54 +0300

    twitter auth (frontend)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,6 +12,43 @@ angular.module('app.controllers')
 
 	})
 
-	.controller('LoginCtrl', function ($scope) {
+	.controller('LoginCtrl', function ($scope, $state, $cordovaOauth, UserService, Config, $ionicPlatform, $ionicLoading, $cordovaPush) {
+		if (UserService.current()) {
+			$state.go('tab.news');
+		}
+		$scope.twitter = function () {
+			$ionicPlatform.ready(function () {
+				$cordovaOauth.twitter(Config.twitterKey, Config.twitterSecret).then(function (result) {
+					$ionicLoading.show({
+						template: 'Loading...'
+					});
+					UserService.login(result).then(function (user) {
+						if (user.deviceToken) {
+							$ionicLoading.hide();
+							$state.go('tab.news');
+							return;
+						}
 
+						$ionicPlatform.ready(function () {
+							$cordovaPush.register({
+								badge: true,
+								sound: true,
+								alert: true
+							}).then(function (result) {
+								UserService.registerDevice({user: user, token: result}).then(function () {
+									$ionicLoading.hide();
+									$state.go('tab.news');
+								}, function (err) {
+									console.log(err);
+								});
+							}, function (err) {
+								console.log('reg device error', err);
+							});
+						});
+					});
+				}, function (error) {
+					console.log('error', error);
+				});
+			});
+		};
 	});
--- www/js/userService.js
+++ www/js/userService.js
@@ -1,16 +1,78 @@
 (function () {
-	function _UserService() {
+	function _UserService($q, config, $http, localStorageService, $state) {
+
+		var user;
+
+		function loginUser(post) {
+			var deferred = $q.defer();
+
+			$http.post(config.server + '/user/login', post)
+				.success(function (data) {
+					if (data.error || !data.user) {
+						deferred.reject(data.error);
+					}
+
+					localStorageService.set('user', data.user);
+					user = data.user;
+
+					deferred.resolve(data.user);
+				})
+				.error(function () {
+					deferred.reject('error');
+				});
+			return deferred.promise;
+		}
+
+		function logoutUser() {
+			localStorageService.remove('user');
+			user = null;
+			$state.go('login');
+		}
+
+		function currentUser() {
+			if (!user) {
+				user = localStorageService.get('user');
+			}
+			return user;
+		}
+
+		function registerDevice(putData) {
+			var deferred = $q.defer();
+
+			$http.put(config.server + '/user/registerDevice', putData)
+				.success(function (data) {
+					if (data.error || !data.user) {
+						deferred.reject(data.error);
+					}
+
+					localStorageService.set('user', data.user);
+					user = data.user;
+
+					deferred.resolve(data.user);
+				})
+				.error(function () {
+					deferred.reject('error');
+				});
+			return deferred.promise;
+		}
 
 		return {
-
+			login: loginUser,
+			logout: logoutUser,
+			current: currentUser,
+			registerDevice: registerDevice
 		};
 	}
 
 	function _ConfigService() {
 		return {
-
+			server: 'http://push-news.herokuapp.com',
+			twitterKey: 'your_twitter_key',
+			twitterSecret: 'your_twitter_secret'
 		};
 	}
+
+	_UserService.$inject = ['$q', 'Config', '$http', 'localStorageService', '$state', '$cordovaPush', '$ionicPlatform'];
 
 	angular.module('app.services')
 		.factory('UserService', _UserService)
